586e0fd2d9347ab66b5df31671a112389fe87e16
Oshan Hamal <oshanhamal3@gmail.com>
2023-03-13 17:55:47 +0545

    API

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import { useState } from "react";
-import ExpenseList from "./expenseTracker/component/ExpenseList";
-import ExpenseFilter from "./expenseTracker/component/ExpenseFilter";
-import ExpenseForm from "./expenseTracker/component/ExpenseForm";
-import catagories from "./expenseTracker/catagories";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [selectedCatagory, setSelectedCatagory] = useState("");
+  const url = "https://jsonplaceholder.typicode.com/users";
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, catagory: "Utilites" },
-    { id: 2, description: "bbb", amount: 10, catagory: "Entertainment" },
-    { id: 3, description: "ccc", amount: 10, catagory: "Groceries" },
-    { id: 4, description: "ddd", amount: 10, catagory: "Utilites" },
-  ]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [err, setErr] = useState("");
 
-  const visibleExpenses = selectedCatagory
-    ? expenses.filter((e) => e.catagory === selectedCatagory)
-    : expenses;
+  useEffect(() => {
+    axios
+      .get<User[]>(url)
+      .then((res) => setUsers(res.data))
+      .catch((err) => setErr(err.message));
+  }, []);
 
   return (
-    <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(newExpense) =>
-            setExpenses([
-              ...expenses,
-              { ...newExpense, id: expenses.length + 1 },
-            ])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter onSelect={(catagory) => setSelectedCatagory(catagory)} />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) =>
-          setExpenses(expenses.filter((expense) => expense.id !== id))
-        }
-      />
-    </div>
+    <>
+      {err && <p className="text-danger">{err}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.id} {user.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
