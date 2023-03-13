20351bf0f4c18b49ab707a3ed3d1c3d335964fca
Oshan Hamal <oshanhamal3@gmail.com>
2023-03-13 18:43:31 +0545

    Loading indication

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -11,22 +11,36 @@ function App() {
 
   const [users, setUsers] = useState<User[]>([]);
   const [err, setErr] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
     axios
-      .get<User[]>(url)
-      .then((res) => setUsers(res.data))
-      .catch((err) => setErr(err.message));
+      .get<User[]>(url, { signal: controller.signal })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setErr(err.message);
+        setLoading(false);
+      });
+
+    return () => {
+      controller.abort;
+    };
   }, []);
 
   return (
     <>
       {err && <p className="text-danger">{err}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
-            {user.id} {user.name}
-          </li>
+          <li key={user.id}>{user.name}</li>
         ))}
       </ul>
     </>
